239ed5007faf7480111275dee0adef8008cd48f3
MRMORNINGSTAR2233 <aksh9881@gmail.com>
2024-06-22 12:37:39 +0530

    waste

--- index.php
+++ index.php
@@ -10,12 +10,6 @@
         <input type="number" id="fnum" name="fnum"><br>
         <label for="fnum2">Second Number:</label>
         <input type="number" id="fnum2" name="fnum2"><br>
-        <label for="symbols">Operators</label>
-        <select name="symbols" id="symb">
-            <option value="+">+</option>
-            <option value="-">-</option>
-            <option value="*">*</option>
-            <option value="/">/</option>
         <input type="submit" value="Submit">
     </form>
     </center>
@@ -23,18 +17,20 @@
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $fnum = $_POST["fnum"];
         $fnum2 = $_POST["fnum2"];
-        $symb = $_POST["symbols"];
-        if ($symb == "+") {
-            $result = $fnum + $fnum2;
-        } elseif ($symb == "-") {
-            $result = $fnum - $fnum2;
-        } elseif ($symb == "*") {
-            $result = $fnum * $fnum2;
-        } elseif ($symb == "/") {
-            $result = $fnum / $fnum2;
+            $result1 = $fnum + $fnum2;
+            $result2 = $fnum - $fnum2;
+            $result3 = $fnum * $fnum2;
+            $result4 = $fnum / $fnum2;
+
+        echo "ADD:\n",$result1;
+
+        echo "SUB:\n",$result2;
+
+        echo "MUL:\n",$result3;
+
+        echo "DIV:\n",$result4;
+
         }
-        echo "<p>The result is: " . $result . "</p>";
-    }
     ?>
 </body>
 </html>
